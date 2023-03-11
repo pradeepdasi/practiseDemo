15d47aff2ea03ddb2ffdff72e8552144b7a88fb4
pradeepdasi <prudhvirajk6@gmail.com>
2023-03-11 13:44:26 +0530

    m1 method created

--- com.data/src/main/java/com/data/practice7.java
+++ com.data/src/main/java/com/data/practice7.java
@@ -1,6 +1,10 @@
 package com.data;
 
 public class practice7 {
+	
+	public void m1() {
+		System.out.println("m1 method");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
